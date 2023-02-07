ab65951e243e9c2661859ce3a20a0ba5c88de509
yyates <yyates@london.edu>
2023-02-07 12:42:23 +0000

    FakePersonRepository and FakePersonRepositoryTests

---
+++ SchoolAPI/Repositories/FakePersonRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolAPI.Repositories
+{
+    public class FakePersonRepository
+    {
+        public int PersonId { get; set; } // do we need it in here?
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public DateTime? EnrollmentDate { get; set;  }
+        public string Discriminator { get; set;  }
+    }
+}
